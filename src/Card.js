ac1360db6a6a600f67be7ce58fbf78a808134b2f
lucp.xyz@gmail.com <lucianopalombo@CL-MMA5110.local>
2018-03-07 14:02:45 -0500

    Category now displayed in card

--- src/Card.js
+++ src/Card.js
@@ -3,18 +3,23 @@ import './Card.css';
 
 class Card extends Component {
 
-  formatCar(str){
+  formatCat(str){
     return str.split("-")[1];
+  }
+  formatTitle(str){
+    return str.split("-")[1].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
   }
 
   handleClick = () => { this.props.clickHandler(this.props.card)};
 
   render() {
 
-    let comClass = "card "+this.formatCar(this.props.card.category);
+    let catClass = "card "+this.formatCat(this.props.card.category);
+    let catTitle = this.formatTitle(this.props.card.category);
 
     return (
-      <button onClick={this.handleClick} className={comClass}>
+      <button onClick={this.handleClick} className={catClass}>
+        <div className="cat"><span>{catTitle}</span></div>
         <p className="name">{this.props.card.name}</p>
         <p className="description">{this.props.card.description}</p>
         {this.props.card.examples !== "" &&
--- src/Roulette.js
+++ src/Roulette.js
@@ -4,19 +4,12 @@ import Card from './Card';
 
 class Roulette extends Component {
 
-  formatTitle(str){
-    return str.split("-")[1].replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-  }
-
   render() {
     return (
       <div className="roulette">
         {this.props.selectedCards.map((card) => {
-          //returns a properly titled category
-          let category = this.formatTitle(card.category);
           return ([
             <div className="roulette-child">
-              <p>{category}</p>
               <Card clickHandler={this.props.clickHandler} key={card.id} card={card} />
             </div>
           ])
