4338ab6accc4715b175176c1ed4bcecdc5c728b6
lucp.xyz@gmail.com <lucianopalombo@CL-MMA5110.local>
2018-04-23 16:08:23 -0400

    Added steps to "Play the game" screen

--- src/Game.js
+++ src/Game.js
@@ -3,11 +3,13 @@ import Roulette from './Roulette';
 import SubmissionField from './SubmissionField';
 
 class Game extends Component {
-
   constructor(props){
     super(props);
-
+    this.state = {
+      step: 1
+    }
     this.handleClick = this.handleClick.bind(this);
+    this.setStep = this.setStep.bind(this);
     this.cardClick = this.cardClick.bind(this);
   }
 
@@ -35,6 +37,14 @@ class Game extends Component {
     this.selectCards();
   }
 
+  setStep(val){
+    console.log(val);
+    this.setState({
+      step: val
+    });
+  }
+
+
   cardClick(e){
     let tempCards = this.props.selectedCards.map((card, index) => {
       if(card === e){
@@ -55,9 +65,36 @@ class Game extends Component {
   render() {
     return (
       <div className="game">
-        <Roulette clickHandler={this.cardClick} selectedCards={this.props.selectedCards} />
-        <button className="roulette-button" onClick={this.handleClick}>Randomise Cards</button>
-        <SubmissionField submitHandler={this.props.submitHandler}/>
+        <div className="flow">
+          <ul>
+            <button onClick={() => {this.setStep(1)}} ><li><span>1</span>Choose a Deck</li></button>
+            <button onClick={() => {this.setStep(2)}} ><li><span>2</span>Create a Combo</li></button>
+            <button onClick={() => {this.setStep(3)}} ><li><span>3</span>Write a Response</li></button>
+            <button onClick={() => {this.setStep(4)}} ><li><span>4</span>Review and Submit</li></button>
+          </ul>
+        </div>
+
+        {this.state.step === 1 &&
+          <div>
+            <h1>Category placeholder</h1>
+          </div>
+        }
+        {this.state.step === 2 &&
+          <div>
+            <Roulette clickHandler={this.cardClick} selectedCards={this.props.selectedCards} />
+            <button className="roulette-button" onClick={this.handleClick}>Randomise Cards</button>
+          </div>
+        }
+        {this.state.step === 3 &&
+          <div>
+            <SubmissionField submitHandler={this.props.submitHandler}/>
+          </div>
+        }
+        {this.state.step === 4 &&
+          <div>
+            <h1>Review placeholder</h1>
+          </div>
+        }
       </div>
     );
   }
