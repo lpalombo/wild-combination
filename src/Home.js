d92cc5e70f38b3a9663440b9ff3f67d265cd8004
lucp.xyz@gmail.com <lucianopalombo@CL-MMA5110.local>
2018-04-24 11:59:32 -0400

    Updates styles on header

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,12 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        <div className="header">
-          <h1>Create the spark for the idea that changes the world.</h1>
-          <p>Wild Combination is a rapid ideation card game where participants apply technologies to real-life scenarios to create novel solutions for people.</p>
-        </div>
+        <header>
+          <div>
+            <h1>Create the spark for the idea that changes the world.</h1>
+            <p>Wild Combination is a rapid ideation card game where participants apply technologies to real-life scenarios to create novel solutions for people.</p>
+          </div>
+        </header>
         <div className="submissions">
           {this.props.submissions.slice(0).reverse().map((submission) => {
             return ([
